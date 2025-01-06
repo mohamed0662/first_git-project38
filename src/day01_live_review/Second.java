33d33e7667ccf350aee746c7579a7f69237de07f
mohamed sahoui <mohamedsahoui062@gmail.com>
2025-01-05 20:23:45 -0500

    updated second class

--- src/day01_live_review/Second.java
+++ src/day01_live_review/Second.java
@@ -4,5 +4,6 @@ public class Second {
     public static void main(String[] args) {
 
         System.out.println("Hello from second class");
+        System.out.println("More changes to second");
     }
 }
